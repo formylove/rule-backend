66cb5d4d486c2bdc0fcbfb14df661e134b1b29b8
formylove <gigglepilate@gmail.com>
2019-02-20 20:49:18 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,7 @@ const express = require('express');
 const Task = require('../models/task.js');
 
 
-
-var task = new Task({
-title:'cervical exercise',
-weight:1
-})
-task.save()
-
-
-var title = 'massage';
-
-Task.find({},(err,docs)=>{
-if(err){
-console.info(err)
-}
-console.info('++++++++++++++++++++++++++++++++++++++++++++++++++'+docs)
-console.info(docs[0].name)
-title=docs[0].title
-})
+var title = "java"
 
 
 var router = express.Router();
